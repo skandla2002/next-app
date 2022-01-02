10c1138b22c4f4e71a77b4fa2c45f60d95a9850b
Andy Pak <skandla2002@gmail.com>
2022-01-02 15:08:36 +0900

    public에 있는 static 이미지 파일 사용

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,21 +5,27 @@ function App() {
   const [name, setName] = useState("");
   const router = useRouter();
   return (
-    <div>
-      <button type="button" onClick={() => router.push("/tomato")}>
-        tomato로 가기
-      </button>
-      <p>이름</p>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        style={{ marginRight: "12px" }}
-      />
-      <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
-        {/* push 함수: 1. pathname, 2. asPath, 3. options를 전달 가능함 */}
-        {name}으로 가기
-      </button>
-    </div>
+    <>
+      <div>
+        <button type="button" onClick={() => router.push("/tomato")}>
+          tomato로 가기
+        </button>
+        <p>이름</p>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          style={{ marginRight: "12px" }}
+        />
+        <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
+          {/* push 함수: 1. pathname, 2. asPath, 3. options를 전달 가능함 */}
+          {name}으로 가기
+        </button>
+      </div>
+      <div>
+        {/*  이미지 : /public에서 /public 경로를 제외한 경로에 이미지로 가져옮*/}
+        <img src="/AI.jpg" alt="AI" />
+      </div>
+    </>
   );
 }
 export default App;
