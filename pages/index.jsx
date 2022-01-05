0186f81ce0c578e272362956576d937c08547fbd
Andy Pak <skandla2002@gmail.com>
2022-01-05 15:21:09 +0900

    Link 존재 여부 값으로 처리 가능 및 설정 변경

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 
+function Child() {
+  return <div>Child</div>;
+}
+
 function App() {
   return (
     <div>
       <h2>Link to 'tomato'</h2>
       <Link href="/tomato">
-        <a>Move to '/tomato'</a>
+        {/* a tag가 있어야 클릭도 되고 history 저정되어 백스페이스바로 선택 가능함 */}
+        {/* <a>Move to '/tomato'</a> */}
+        {/* 컴포넌트가 내부에 있는 경우는 동작하지 않음  */}
+        {/* <Child />  */}
+        {/* 일반 태그는 동작함 */}
+        <div>tag</div>
       </Link>
     </div>
   );
