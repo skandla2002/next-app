27cc01bf8df645a61039c5ec8906bfd3595be08f
Andy Pak <skandla2002@gmail.com>
2022-01-07 11:37:14 +0900

    fetch 호출

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,22 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
 
-function App() {
-  const [name, setName] = useState("");
-  const router = useRouter();
-  return (
-    <>
-      <div>
-        <button type="button" onClick={() => router.push("/tomato")}>
-          tomato로 가기
-        </button>
-        <p>이름</p>
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          style={{ marginRight: "12px" }}
-        />
-        <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
-          {/* push 함수: 1. pathname, 2. asPath, 3. options를 전달 가능함 */}
-          {name}으로 가기
-        </button>
-      </div>
-      <div>
-        {/*  이미지 : /public에서 /public 경로를 제외한 경로에 이미지로 가져옮*/}
-        <img src="/AI.jpg" alt="AI" />
-      </div>
-    </>
-  );
-}
-export default App;
+const index = ({ user }) => {
+  const username = user && user.name;
+  return <div>{username}</div>;
+};
+
+export const getSererSideProps = async () => {
+  try {
+    const res = await fetch("https://api.github.com/users/jerrynim");
+    if (res.status === 200) {
+      const user = await res.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+};
+
+export default index;
