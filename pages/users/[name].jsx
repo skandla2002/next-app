ac870aea36a406fb7d6c09a5d40e71b0a4236712
skandla2002 <skandla2002@gmail.com>
2022-01-10 23:38:57 +0900

    style처리

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,50 +1,89 @@
 import fetch from "isomorphic-unfetch";
-import css from 'styled-jsx/css';
+import css from "styled-jsx/css";
 
 // nested는 지원 하지 않음
 const style = css`
-  h2{
-    margin-left: 20px;
+  .profile-box {
+    width: 25%;
+    max-width: 272px;
+    margin-right: 26px;
   }
-  .user-bio {
-    margin-top: 12px;
-    font-style: italic;
+  .profile-image-wrapper {
+    width: 100%;
+    border: 1px solid #e1e4e8;
+  }
+  .profile-image-wrapper .profile-image {
+    display: block;
+    width: 100%;
+  }
+  .profile-username {
+    margin: 0;
+    padding-top: 16px;
+    font-size: 26px;
+  }
+  .profile-user-login {
+    margin: 0;
+    font-size: 20px;
+  }
+  .profile-user-bio {
+    margin: 0;
+    padding-top: 16px;
+    font-size: 14px;
   }
 `;
 
-const username = ({ user }) => (
-  <>
-    {user ? (
-      <div>
-        <h2>{user.name}</h2>
-        <p className="user-bio">{user.bio}</p>
+// const username = ({ user }) => (
+//   <>
+//     {user ? (
+//       <div>
+//         <h2>{user.name}</h2>
+//         <p className="user-bio">{user.bio}</p>
+//       </div>
+//     ) : (
+//       <div>유저정보가 없습니다.</div>
+//     )}
+//     <style jsx>{style}</style>
+//   </>
+// );
+
+const name = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+  return (
+    <>
+      <div className="profile-box">
+        <div className="profile-image-wrapper">
+          <img
+            src={user.avatar_url}
+            alt={`${user.name} 프로필 이미지`}
+            className="profile-image"
+          />
+        </div>
+        <h2 className="profile-username">{user.name}</h2>
+        <p className="profile-user-login">{user.login}</p>
+        <p className="profile-user-bio">{user.bio}</p>
       </div>
-    ) : (
-      <div>유저정보가 없습니다.</div>
-    )}
-    <style jsx>{style}</style>
-  </>
-);
+      <style jsx>{style}</style>
+    </>
+  );
+};
 
-// const name = ({ user }) => {
-//   const username = user && user.name;
-//   return <div>{username}</div>;
-// };
-
-// name.getInitialProps = async ({ query }) => {
-//   console.log(query);
-//   const { name } = query;
-//   try {
-//     const res = await fetch(`https://api.github.com/users/${name}`);
-//     if (res.status === 200) {
-//       const user = await res.json();
-//       return { props: { user } };
-//     }
-//     return { props: {} };
-//   } catch (e) {
-//     console.log(e);
-//     return { props: {} };
-//   }
-// };
+export const getSererSideProps = async ({ query }) => {
+  console.log(query);
+  const { name } = query;
+  try {
+    const res = await fetch(`https://api.github.com/users/${name}`);
+    if (res.status === 200) {
+      console.log(res);
+      const user = await res.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+};
 
 export default name;
